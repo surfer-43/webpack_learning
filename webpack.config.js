0316a43643843c3b72286ddc50829ff337db7fd2
Joel Brown <surfer_43@hotmail.com>
2020-04-28 16:19:03 -0400

    modifications in trouble shooting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "/source/index.sj",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js", 
-        publicpath: ""
+        filename: "bundle.js",
+        publicPath: ""
     },
     devtool: "cheap-module-eval-source-map",
     module: {
@@ -38,7 +39,18 @@ module.exports = {
                         }
                  }
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif$)/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.html",
+            filename: "index.html",
+            inject: "body"
+        })
+    ]
 };
